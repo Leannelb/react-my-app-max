3c8745c25d0374ac739040e64739f36aefe8802c
leannelb <leannelaceybyrne@outlook.com>
2021-09-29 20:43:58 +0100

    ids added to array

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -4,18 +4,18 @@ import Chart from '../Chart/Chart';
 
 const ExpensesChart = props => {
     const chartDataPoints = [
-        { label: 'Jan', value: 0 },
-        { label: 'Feb', value: 1 },
-        { label: 'Mar', value: 2 },
-        { label: 'Apr', value: 3 },
-        { label: 'May', value: 4 },
-        { label: 'Jun', value: 5 },
-        { label: 'Jul', value: 6 },
-        { label: 'Aug', value: 7 },
-        { label: 'Sep', value: 8 },
-        { label: 'Oct', value: 9 },
-        { label: 'Nov', value: 10 },
-        { label: 'Dec', value: 11 },
+        { label: 'Jan', value: 0, id: 0 },
+        { label: 'Feb', value: 1, id: 1 },
+        { label: 'Mar', value: 2, id: 2 },
+        { label: 'Apr', value: 3, id: 3 },
+        { label: 'May', value: 4, id: 4 },
+        { label: 'Jun', value: 5, id: 5 },
+        { label: 'Jul', value: 6, id: 6 },
+        { label: 'Aug', value: 7, id: 7 },
+        { label: 'Sep', value: 8, id: 8 },
+        { label: 'Oct', value: 9, id: 9 },
+        { label: 'Nov', value: 10, id: 10 },
+        { label: 'Dec', value: 11, id: 11 },
     ];
 
     for (const expense of props.expense) {
